3dda28f583386790e424c773857403282e342d58
Matias Aguero <mjaguero.work@gmail.com>
2018-09-03 11:12:28 -0300

    can modify textview text on rutime

--- app/src/main/java/com/hackaton/endava/calendar/CalendarActivity.java
+++ app/src/main/java/com/hackaton/endava/calendar/CalendarActivity.java
@@ -2,7 +2,6 @@ package com.hackaton.endava.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.core.Frame;
