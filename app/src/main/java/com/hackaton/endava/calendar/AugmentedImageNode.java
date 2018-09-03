737502a9905e1f8f1cf319b1f84d20a742fa56a5
Matias Aguero <mjaguero.work@gmail.com>
2018-09-03 11:12:28 -0300

    can modify textview text on rutime

--- app/src/main/java/com/hackaton/endava/calendar/AugmentedImageNode.java
+++ app/src/main/java/com/hackaton/endava/calendar/AugmentedImageNode.java
@@ -2,16 +2,13 @@ package com.hackaton.endava.calendar;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.ar.core.AugmentedImage;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Vector3;
-import com.google.ar.sceneform.rendering.Color;
-import com.google.ar.sceneform.rendering.MaterialFactory;
-import com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.rendering.Renderable;
-import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.rendering.ViewRenderable;
 
 import java.util.concurrent.CompletableFuture;
@@ -24,29 +21,13 @@ public class AugmentedImageNode extends AnchorNode {
     private AugmentedImage image;
 
     private static CompletableFuture<ViewRenderable> calendarView;
-    private static CompletableFuture<Void> sphere;
-    private static Renderable redSphereRenderable;
 
 
     public AugmentedImageNode(Context context) {
         // Upon construction, start loading the models for the corners of the frame.
 
-        if (sphere == null) {
-            sphere = MaterialFactory.makeOpaqueWithColor(context, new Color(android.graphics.Color.RED))
-                    .thenAccept(
-                            material -> {
-                                redSphereRenderable = ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.15f, 0.0f), material);
-                            });
-        }
-
         if (calendarView == null) {
             calendarView = ViewRenderable.builder().setView(context, R.layout.calendar_view).build();
-//            try {
-//                calendarView.getNow(null).getView().;
-//            } catch (Exception e) {
-//                // TODO
-//            }
-
         }
 
     }
@@ -62,16 +43,6 @@ public class AugmentedImageNode extends AnchorNode {
         this.image = image;
 
         // If any of the models are not loaded, then recurse when all are loaded.
-//        if (!calendarView.isDone()) {
-//            CompletableFuture.allOf(calendarView)
-//                    .thenAccept((Void aVoid) -> setImage(image))
-//                    .exceptionally(
-//                            throwable -> {
-//                                Log.e(TAG, "Exception loading", throwable);
-//                                return null;
-//                            });
-//        }
-
         if (!calendarView.isDone()) {
             CompletableFuture.allOf(calendarView)
                     .thenAccept((Void aVoid) -> setImage(image))
@@ -82,6 +53,13 @@ public class AugmentedImageNode extends AnchorNode {
                             });
         }
 
+        ViewRenderable now = calendarView.getNow(null);
+        if (now != null) {
+            LinearLayout layout = (LinearLayout) now.getView();
+            TextView tittle = layout.findViewById(R.id.tittle);
+            tittle.setText(image.getName());
+        }
+
         // Set the anchor based on the center of the image.
         setAnchor(image.createAnchor(image.getCenterPose()));
 
@@ -89,15 +67,10 @@ public class AugmentedImageNode extends AnchorNode {
         Vector3 localPosition = new Vector3();
         localPosition.set(0.0f , 0.0f, 0.0f);
 
-//        Node sphereNode = new Node();
-//        sphereNode.setParent(this);
-//        sphereNode.setLocalPosition(localPosition);
-//        sphereNode.setRenderable(redSphereRenderable);
-
         Node viewNode = new Node();
         viewNode.setParent(this);
         viewNode.setLocalPosition(localPosition);
-        viewNode.setRenderable(calendarView.getNow(null));
+        viewNode.setRenderable(now);
     }
 
     public AugmentedImage getImage() {
